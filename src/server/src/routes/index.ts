9e7611261919fc22aea82481a108b398bd28f18b
sashauly <sashauly.code@ya.ru>
2023-12-10 17:14:27 +0300

    [server]: remove previous routes

--- src/server/src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-const router = Router();
-
-router.get('/', function (req, res) {
-  res.json({ message: 'Hello, Express TypeScript!' });
-});
-
-export default router;
--- src/server/src/routes/users.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const router = express.Router();
-
-router.get('/', function (req, res) {
-  res.json({ message: 'This is Users' });
-});
-
-export default router;
