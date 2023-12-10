42ad4bd1f109e105c2eb4248d17f44aff13aaee5
sashauly <sashauly.code@ya.ru>
2023-12-10 17:15:29 +0300

    [server]: add routes to express app

--- src/server/src/index.ts
+++ src/server/src/index.ts
@@ -1,27 +1,20 @@
 import express, { Application } from 'express';
-import path from 'path';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import router from './routes/vacancy.routes';
+import userRouter from './routes/user.routes';
 
-import indexRouter from './routes/index';
-import usersRouter from './routes/users';
-
-//For env File
 dotenv.config();
 
 const app: Application = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.get('/api/v1', (req, res) => {
-  res.json({ message: 'Api v1' });
-});
+app.use('/', router);
+app.use('/users', userRouter);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
