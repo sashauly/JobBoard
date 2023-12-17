abbb0e0b1e9eb39767691c605e56eaebf0bfee3e
sashauly <sashauly.code@ya.ru>
2023-12-17 14:59:29 +0300

    [init]: init gihub repo

--- src/server/src/auth/index.ts
+++
@@ -1,6 +0,0 @@
-async function authenticate(req, res, next) {
-  const token = req.headers.authorization;
-  const payload = jwt.verify(token, 'your-secret-key'); // Verify JWT token
-  req.user = await prisma.user.findUnique({ where: { id: payload.userId } });
-  next();
-}
