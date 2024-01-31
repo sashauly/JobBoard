bfeb882df81d4581c8071014575ae79f5e2e6424
sashauly <sashauly.code@ya.ru>
2024-01-31 21:04:00 +0300

    add: route paths

---
+++ src/server/src/constants/Paths.ts
@@ -0,0 +1,18 @@
+/**
+ * Express routes paths
+ */
+export default {
+  Base: "/api",
+  Auth: {
+    Base: "/auth",
+    Register: "/register",
+    Login: "/login",
+    Logout: "/logout",
+  },
+  Users: {
+    Base: "/users",
+  },
+  Vacancies: {
+    Base: "/vacancies",
+  }
+};
