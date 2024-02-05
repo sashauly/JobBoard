8e5dff089892809b0fb46949115413ac87bb173d
sashauly <sashauly.code@ya.ru>
2024-02-05 20:50:41 +0300

    fix: default port to 5000

--- src/server/src/index.ts
+++ src/server/src/index.ts
@@ -1,5 +1,5 @@
 import server from "./server";
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 server.listen(port, () => console.log(`Server running on port ${port}`));
