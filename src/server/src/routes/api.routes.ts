1f385be4831b8e7b1eae0fcab1e379d427a251fb
sashauly <sashauly.code@ya.ru>
2024-02-05 20:52:50 +0300

    add: checkauth middleware to user route

--- src/server/src/routes/api.routes.ts
+++ src/server/src/routes/api.routes.ts
@@ -5,11 +5,12 @@ import Paths from "../constants/Paths";
 import authRouter from "./auth.routes";
 import userRouter from "./user.routes";
 import vacancyRouter from "./vacancy.routes";
+import checkAuth from "../middleware/checkAuth";
 
 const apiRouter = Router();
 
 apiRouter.use(Paths.Auth.Base, authRouter);
-apiRouter.use(Paths.Users.Base, userRouter);
+apiRouter.use(Paths.Users.Base, checkAuth, userRouter);
 apiRouter.use(Paths.Vacancies.Base, vacancyRouter);
 
 export default apiRouter;
