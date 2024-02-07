4e4d96f4c06dd58680c3f6cfcf777448764b253a
sashauly <sashauly.code@ya.ru>
2024-02-07 14:05:59 +0300

    add ensureRole middleware to vacancies route

--- src/server/src/routes/vacancy.routes.ts
+++ src/server/src/routes/vacancy.routes.ts
@@ -1,13 +1,31 @@
 import { Router } from "express";
 import vacancyController from "../controllers/vacancy.controller";
+import ensureRole from "../middleware/ensureRole";
+import { Role } from "@prisma/client";
 
 const vacancyRouter = Router();
 
 vacancyRouter.get("/", vacancyController.getAllVacancies);
 vacancyRouter.get("/:id", vacancyController.getVacancyById);
-vacancyRouter.post("/", vacancyController.createVacancy);
-vacancyRouter.put("/:id", vacancyController.updateVacancy);
-vacancyRouter.patch("/:id", vacancyController.changeStatusVacancy);
-vacancyRouter.delete("/:id", vacancyController.deleteVacancy);
+vacancyRouter.post(
+  "/",
+  ensureRole(Role.EMPLOYER),
+  vacancyController.createVacancy
+);
+vacancyRouter.put(
+  "/:id",
+  ensureRole(Role.EMPLOYER),
+  vacancyController.updateVacancy
+);
+vacancyRouter.patch(
+  "/:id",
+  ensureRole(Role.EMPLOYER),
+  vacancyController.changeStatusVacancy
+);
+vacancyRouter.delete(
+  "/:id",
+  ensureRole(Role.EMPLOYER),
+  vacancyController.deleteVacancy
+);
 
 export default vacancyRouter;
